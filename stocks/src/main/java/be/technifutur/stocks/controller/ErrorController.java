75721bdea361b0e5ae7e5e0cfe42ea06564cc1fa
sbstnlambert <sbstn.lambert@gmail.com>
2022-05-02 11:44:13 +0200

    added invalid quantity exception handler

--- stocks/src/main/java/be/technifutur/stocks/controller/ErrorController.java
+++ stocks/src/main/java/be/technifutur/stocks/controller/ErrorController.java
@@ -1,15 +1,18 @@
 package be.technifutur.stocks.controller;
 
 import be.technifutur.stocks.exception.ElementNotFoundException;
+import be.technifutur.stocks.exception.InvalidQuantityException;
 import be.technifutur.stocks.model.dto.ErrorDTO;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
 
+@ControllerAdvice
 public class ErrorController extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(ElementNotFoundException.class)
@@ -20,6 +23,19 @@ public class ErrorController extends ResponseEntityExceptionHandler {
                                 .message(e.getMessage())
                                 .method(HttpMethod.resolve(request.getMethod()))
                                 .status(404)
+                                .uri(request.getRequestURI())
+                                .build()
+                );
+    }
+
+    @ExceptionHandler(InvalidQuantityException.class)
+    public ResponseEntity<ErrorDTO> handleElementNotFound(InvalidQuantityException e, HttpServletRequest request) {
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY)
+                .body(
+                        ErrorDTO.builder()
+                                .message(e.getMessage())
+                                .method(HttpMethod.resolve(request.getMethod()))
+                                .status(422)
                                 .uri(request.getRequestURI())
                                 .build()
                 );
