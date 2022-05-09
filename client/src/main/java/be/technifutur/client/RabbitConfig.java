bcb4e5e48bb35a7fe8a6666e47f6f47b20ed4414
JMF <jm.ficart@gmail.com>
2022-05-09 14:17:12 +0200

    09-05-2022 14:14 JMF Insertion code Rabbit de Grégoire

---
+++ client/src/main/java/be/technifutur/client/RabbitConfig.java
@@ -0,0 +1,29 @@
+package be.technifutur.client;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfig {
+
+    @Bean("client_queue")
+    public Queue clientQueue(){
+        return new Queue("client_queue", true);
+    }
+
+    @Bean
+    public DirectExchange directExchange(){
+        return new DirectExchange("microservice.direct");
+    }
+
+    @Bean
+    public Binding clientBinding(DirectExchange exchange, @Qualifier("client_queue") Queue queue){
+        return BindingBuilder.bind(queue).to(exchange).with("client");
+    }
+}
